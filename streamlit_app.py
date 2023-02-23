db2a3fbea697e40bc2092ee1c8aeac31c33063ef
Sharma-GI <126136446+Sharma-GI@users.noreply.github.com>
2023-02-24 02:51:54 +1100

    Update streamlit_app.py
    add stop to end line

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,6 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows=get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-#stop below this
-streamlit.stop()
 
 #Allow end user to add a fruit to list
 def insert_row_snowflake(new_fruit):
@@ -71,4 +69,6 @@ if streamlit.button('Add a fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
+#stop below this
+streamlit.stop()
 
